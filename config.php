9219949da10d0753885c4c45d317d117036a6af0
Echizen22 <erixon.arce.justiniano@gmail.com>
2026-04-13 22:31:26 +0200

    Fix puerto de base de datos a 3306 (puerto por defecto MySQL)

--- config.php
+++ config.php
@@ -3,6 +3,6 @@ define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '1234');
 define('DB_NAME', 'gestion_eventos');
-define('DB_PORT', 3307);
+define('DB_PORT', 3306);
 define('BASE_URL', '/cwu');
 ?>
